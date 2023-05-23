5c03fc91fc578040f16457b684722a7052cd02fd
HarlynsonSMagalhaes <harlynson.magalhaes@zipdin.com.br>
2023-05-23 18:41:01 -0300

    feat: add group pages

---
+++ src/add-group/hours/index.js
@@ -0,0 +1,43 @@
+var hour = "";
+var hourElement = null;
+var minute = "";
+var minuteElement = null;
+
+function init() { }
+
+function checkMarker(type, element) {
+
+    if (type == "hour") {
+
+        if (element.childNodes[0] != hour) {
+            hour = element.childNodes[0];
+            element.childNodes[1].setAttribute("marked", "true");
+
+            if (null != hourElement) {
+                hourElement.childNodes[1].removeAttribute("marked");
+            }
+
+            hourElement = element;
+        } else {
+            hour = "";
+            element.childNodes[1].removeAttribute("marked");
+        }
+
+    } else if (type == "minute") {
+
+        if (element.childNodes[0] != minute) {
+            minute = element.childNodes[0];
+            element.childNodes[1].setAttribute("marked", "true");
+
+            if (null != minuteElement) {
+                minuteElement.childNodes[1].removeAttribute("marked");
+            }
+
+            minuteElement = element;
+        } else {
+            minute = "";
+            element.childNodes[1].removeAttribute("marked");
+        }
+
+    }
+}
---
+++ src/add-group/modality/index.js
@@ -0,0 +1,22 @@
+var modality = "";
+var checkedElement = null;
+
+function init() { }
+
+function checkMarker(element) {
+
+    if (element.childNodes[0] != modality) {
+        modality = element.childNodes[0];
+        element.childNodes[1].setAttribute("marked", "true");
+
+        if (null != checkedElement) {
+            checkedElement.childNodes[1].removeAttribute("marked");
+        }
+
+        checkedElement = element;
+    } else {
+        modality = "";
+        element.childNodes[1].removeAttribute("marked");
+    }
+
+}
