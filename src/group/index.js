71b17071f768983808e8e43ee7054d4b2c6c3828
HarlynsonSMagalhaes <harlynson.magalhaes@zipdin.com.br>
2023-06-04 19:45:52 -0300

    feat: add leave group

--- src/group/index.js
+++ src/group/index.js
@@ -1,3 +1,6 @@
+var joined = false;
+
+
 function init () {
     var options = {
         center: new google.maps.LatLng(-19.923444, -43.934890),
@@ -18,9 +21,9 @@ function init () {
             },
         ],
     };
-
+    
     var map = new google.maps.Map(document.getElementById("map"), options);
-
+    
     var marker = new google.maps.Marker({
         position: { lat: -19.923761, lng: -43.934267},
         map,
@@ -28,6 +31,21 @@ function init () {
         icon: {
             url: "../assets/get-alt-fill.svg",
             scaledSize: new google.maps.Size(40, 40)
+        }
+    });
+    
+    var joinGroup = document.getElementById("enter-group");
+    joinGroup.addEventListener("click", () =>{
+        if (!joined) {
+            joinGroup.innerHTML = "Sair do grupo";
+            joinGroup.style.background = getComputedStyle(document.documentElement).getPropertyValue("--red");
+            joinGroup.style.color = getComputedStyle(document.documentElement).getPropertyValue("--white");
+            joined = true;
+        } else {
+            joinGroup.innerHTML = "Entrar no grupo";
+            joinGroup.style.background = getComputedStyle(document.documentElement).getPropertyValue("--light-green");
+            joinGroup.style.color = getComputedStyle(document.documentElement).getPropertyValue("--dark-blue");
+            joined = false;
         }
     });
     
