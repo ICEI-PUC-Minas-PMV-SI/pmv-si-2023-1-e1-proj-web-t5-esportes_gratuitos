08ad36a2ec3c1e478d60276451160a39bc864636
lucasperes420 <112734941+lucasperes420@users.noreply.github.com>
2023-06-23 12:52:26 -0300

    funcionalidade recuperar senha

--- src/recovery/index.js
+++ src/recovery/index.js
@@ -10,3 +10,44 @@ buttonCodigo.addEventListener("click",()=>{
     modal.classList.toggle("active")
     shadow.classList.toggle("active")
  })
+
+ var codigoAleatorio=Math.floor(Math.random() * 9999) +1
+ function enviarCodigo(){
+   var recuperarEmail=document.getElementById("recuperarEmail").value
+   
+   var listaUsuario=JSON.parse(localStorage.getItem("lista_usuarios"))
+   for(var i in listaUsuario){
+      var entry = listaUsuario[i]
+      if(recuperarEmail==entry.email){
+         console.log("email encontrado")
+        
+         console.log(codigoAleatorio)
+      }else{
+         alert("email nao encontrado")
+      
+         modal.style.display="none"
+         shadow.style.display="none"
+
+      }
+   }
+ }
+function confirmarCodigo(){
+   var inputCodigo=document.getElementById("inputCodigo").value
+var novaSenha=document.getElementById("novaSenha").value
+if(codigoAleatorio==inputCodigo){
+   console.log("codigo confirmado")
+   var listaUsuario=JSON.parse(localStorage.getItem("lista_usuarios"))
+   for(var i in listaUsuario){
+      var entry = listaUsuario[i]
+      console.log(entry.senha)
+      entry.senha=novaSenha
+      console.log(entry.senha)
+      localStorage.setItem("lista_usuarios",JSON.stringify(entry.senha))
+      //entry.senha=novaSenha
+      //localStorage.setItem("lista_usuarios",novaSenha)
+      
+
+   }
+
+}
+}
